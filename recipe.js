afb2654caac08fae7c6028049384d92c663f20e2
Pooja0629 <poojashree2266@gmail.com>
2025-09-17 09:25:31 -0700

    Add files via upload

---
+++ recipe.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const mongoURL = "mongodb+srv://ReceipeUser:Pooja2906@receipe.hnkhrxa.mongodb.net/recipesDB?retryWrites=true&w=majority";
+mongoose.connect(mongoURL)
+  .then(() => console.log(" MongoDB connected"))
+  .catch(err => console.log("MongoDB error:", err));
+
+const recipeSchema = new mongoose.Schema({
+  name: String,
+  ingredients: [String],
+  steps: String,
+  category: String
+});
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String
+});
+const User = mongoose.model("User", userSchema);
+
+
+app.post("/signup", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+
+    res.json({ message: "User registered successfully", user: newUser });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email, password });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    res.json({ message: `Welcome back, ${user.username}!`, user });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+
