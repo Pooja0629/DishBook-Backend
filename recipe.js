bf641f30ffff59875f41d28e56ed0bd7a12c97cc
Pooja0629 <poojashree2266@gmail.com>
2025-09-18 19:31:53 +0530

    Update recipe.js

--- recipe.js
+++ recipe.js
@@ -80,6 +80,11 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("Backend is running ✅");
+});
+
+
 
 
 
